3fdedcbe8dd192f06a5d972be57ab5e176077380
SNOH <116703016+snoh6839@users.noreply.github.com>
2023-06-15 14:18:04 +0900

    compser facker

--- Finmate/database/seeders/DatabaseSeeder.php
+++ Finmate/database/seeders/DatabaseSeeder.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-
+use Facker\Factory as Facker;
 class DatabaseSeeder extends Seeder
 {
     /**
