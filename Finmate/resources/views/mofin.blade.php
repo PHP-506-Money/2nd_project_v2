e1d6cc035d9ad9904bcf66f39f8c0f46e2a87d4a
Choihyukjae <wo4013@nave.com>
2023-06-19 10:42:55 +0900

    0619 udt 최혁재

--- Finmate/resources/views/mofin.blade.php
+++ Finmate/resources/views/mofin.blade.php
@@ -5,6 +5,24 @@
 @section('contents')
 
 <h1>나의 모핀이</h1>
-{{$data->point}}
+<span>나의 포인트 : {{$data->point}}</span> <br>
 
+<form action="{{route('mofin.point')}}" method="post">
+    @csrf
+<button onclick="pt()" type="submit">랜덤포인트</button>
+</form>
+<br><br>
+
+<form action="{{route('mofin.item')}}" method="post">
+    @csrf
+<button type="submit">랜덤아이템</button>
+</form>
+@if(isset($pt1))
+    <script>
+        // 페이지가 로드될 때 자동으로 실행되도록 수정
+        window.addEventListener('DOMContentLoaded', function() {
+            alert('축하합니다 {{ $pt1 }} 당첨!!' );
+        });
+    </script>
+@endif
 @endsection
