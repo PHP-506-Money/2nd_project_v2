c38b631579bfdb69f94fd8eb342f061234d8edab
SNOH <116703016+snoh6839@users.noreply.github.com>
2023-06-20 11:05:38 +0900

    transacrion list v1

--- Finmate/resources/views/layout/header.blade.php
+++ Finmate/resources/views/layout/header.blade.php
@@ -7,10 +7,14 @@
         <h1>@yield('header', 'header')</h1>
         @auth
         <nav class="nav">
-            <a href="{{url('/assets'.'/'.auth()->user()->userid)}}" class="nav-item {{ Route::current('/assets'.'/'.auth()->user()->userid) ? 'is-active' : '' }}" active-color="#FF7676">자산</a>
-            <a href="{{url('/mofin'.'/'.auth()->user()->userno)}}" class="nav-item {{ Route::current('/mofin'.'/'.auth()->user()->userno) ? 'is-active' : '' }}" active-color="#FF7676">모핀</a>
-            <a href="{{url('/goal'.'/'.auth()->user()->userno)}}" class="nav-item {{ Route::current('/goal'.'/'.auth()->user()->userno) ? 'is-active' : '' }}" active-color="#FF7676">목표</a>
-            <a href="{{url('/budget'.'/'.auth()->user()->userid)}}" class="nav-item {{ Route::current('/budget'.'/'.auth()->user()->userid) ? 'is-active' : '' }}" active-color="#FF7676">예산</a>
+            <a href="{{url('/assets'.'/'.auth()->user()->userid)}}" class="nav-item {{ Request::url('/assets'.'/'.auth()->user()->userid) ? 'is-active' : '' }}" active-color="#FF7676">자산</a>
+
+            <a href="{{url('/mofin'.'/'.auth()->user()->userno)}}" class="nav-item {{ Request::url('/mofin'.'/'.auth()->user()->userno) ? 'is-active' : '' }}" active-color="#FF7676">모핀</a>
+
+            <a href="{{url('/goal'.'/'.auth()->user()->userno)}}" class="nav-item {{ Request::url('/goal'.'/'.auth()->user()->userno) ? 'is-active' : '' }}" active-color="#FF7676">목표</a>
+
+            <a href="{{url('/budget'.'/'.auth()->user()->userid)}}" class="nav-item {{ Request::url('/budget'.'/'.auth()->user()->userid) ? 'is-active' : '' }}" active-color="#FF7676">예산</a>
+
 
             <a href="{{ route('users.myinfo') }}" class="nav-item {{ Request::is('users/myinfo') ? 'is-active' : '' }}" active-color="#FF7676">Myinfo</a>
             <a href="{{ route('users.logout') }}" class="nav-item {{ Request::is('users/logout') ? 'is-active' : '' }}" active-color="#FF7676">logout</a>
