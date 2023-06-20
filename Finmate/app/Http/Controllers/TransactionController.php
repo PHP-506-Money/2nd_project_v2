5f9c67139ae22119705ddfa499b2b705f7abb748
SNOH <116703016+snoh6839@users.noreply.github.com>
2023-06-20 12:42:06 +0900

    transaction list com

--- Finmate/app/Http/Controllers/TransactionController.php
+++ Finmate/app/Http/Controllers/TransactionController.php
@@ -10,9 +10,30 @@ class TransactionController extends Controller
     public function index($userid)
     {
         $transactions = Transaction::join('assets', 'transactions.assetno', '=', 'assets.assetno')
-                        ->select('transactions.*')
-                        ->where('assets.userid', $userid)
-                        ->get();
-        return view('transactions', ['transactions' => $transactions]);
+            ->select('transactions.*')
+            ->where('assets.userid', $userid)
+            ->orderBy('trantime', 'desc') // 거래일시를 기준으로 내림차순 정렬
+            ->get();
+
+        // 월별 총 수입 및 지출 계산
+        $monthly_income = [];
+        $monthly_expense = [];
+
+        foreach ($transactions as $transaction) {
+            $date = date('Y-m', strtotime($transaction->trantime));
+            if ($transaction->type == 0) {
+                $monthly_income[$date] = ($monthly_income[$date] ?? 0) + $transaction->amount;
+            } else {
+                $monthly_expense[$date] = ($monthly_expense[$date] ?? 0) + $transaction->amount;
+            }
+        }
+
+        return view('transactions', [
+            'transactions' => $transactions,
+            'monthly_income' => $monthly_income,
+            'monthly_expense' => $monthly_expense
+        ]);
     }
 }
+
+
--- Finmate/resources/views/transactions.blade.php
+++ Finmate/resources/views/transactions.blade.php
@@ -5,6 +5,18 @@
 @section('header', 'MY TRANSACTIONS')
 
 @section('contents')
+<!-- 좌우 버튼 -->
+
+<h3>
+    <button id="previous-month-btn">&lt;</button><span id="current-month"></span>월<button id="next-month-btn">&gt;</button>
+    <br>
+    총 수입: <span id="monthly-income"></span> 원
+    <br>
+    총 지출: <span id="monthly-expense"></span> 원
+
+</h3>
+
+
 
 <div class="listbox">
     <a href="{{url('/assets'.'/'.auth()->user()->userid)}}">내 자산 목록 보러가기</a>
@@ -17,48 +29,94 @@
             <th>거래일시</th>
         </tr>
         @foreach($transactions as $tran)
-        <tr>
-
+        <tr data-month="{{ substr($tran->trantime, 0, 7) }}">
             @if($tran->type == '0')
-                <td>입금</td>
+            <td>입금</td>
             @else
-                <td>출금</td>
+            <td>출금</td>
             @endif
-            
+
             <td>{{$tran->payee}}</td>
             @if($tran->type == '0')
             <td>수입</td>
             @else
             <td>{{$tran->char}}</td>
-
             @endif
 
-            
             @if($tran->type == '0')
             <td>{{number_format($tran->amount)}}원</td>
-
             @else
             <td>-{{number_format($tran->amount)}}원</td>
-
             @endif
 
-            
             <td>{{$tran->trantime}}</td>
-
         </tr>
-
         @endforeach
-
-
     </table>
-
-    
-
 </div>
 
+<script>
+    const currentMonthElem = document.getElementById("current-month");
+    const monthlyIncomeElem = document.getElementById("monthly-income");
+    const monthlyExpenseElem = document.getElementById("monthly-expense");
+    const previousMonthBtn = document.getElementById("previous-month-btn");
+    const nextMonthBtn = document.getElementById("next-month-btn");
+    const allRows = document.querySelectorAll(".listbox tr");
 
+    const monthlyIncome = JSON.parse('@json($monthly_income)'.replace(/&quot;/g, '\"'));
+    const monthlyExpense = JSON.parse('@json($monthly_expense)'.replace(/&quot;/g, '\"'));
+
+    const today = new Date();
+    let currentYear = today.getFullYear();
+    let currentMonth = today.getMonth() + 1;
+    currentMonth = currentMonth < 10 ? '0' + currentMonth : currentMonth;
+
+    const updateMonthElem = () => {
+        const monthStr = `${currentYear}-${currentMonth}`;
+        currentMonthElem.textContent = monthStr;
+        showByMonth(monthStr);
+        monthlyIncomeElem.textContent = monthlyIncome[monthStr] || 0;
+        monthlyExpenseElem.textContent = monthlyExpense[monthStr] || 0;
+    };
+
+    const showByMonth = (month) => {
+        allRows.forEach((row) => {
+            if (row.dataset.month === month) row.style.display = "table-row";
+            else row.style.display = "none";
+        });
+    };
+
+    // 초기 표시
+    updateMonthElem();
+
+   // 이전 달로 이동
+previousMonthBtn.addEventListener("click", () => {
+currentMonth = parseInt(currentMonth, 10) - 1;
+if (currentMonth < 1) { currentMonth=12; currentYear--; } currentMonth=currentMonth < 10 ? '0' + currentMonth : currentMonth; updateMonthElem(); });
+
+
+
+// 다음 달로 이동
+nextMonthBtn.addEventListener("click", () => {
+currentMonth = parseInt(currentMonth, 10) + 1;
+if (currentMonth > 12) {
+currentMonth = 1;
+currentYear++;
+}
+currentMonth = currentMonth < 10 ? '0' + currentMonth : currentMonth; updateMonthElem(); });
+
+
+
+
+
+
+</script>
 
 @endsection
+
+
+
+
 
 
 
