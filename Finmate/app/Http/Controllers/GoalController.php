f8b156c73db9516925af86e47079389006f5b335
Choihyukjae <wo4013@naver.com>
2023-06-20 12:41:41 +0900

    0620 udt 최혁재

--- Finmate/app/Http/Controllers/GoalController.php
+++ Finmate/app/Http/Controllers/GoalController.php
@@ -8,10 +8,16 @@ use Illuminate\Support\Facades\DB;
 
 class GoalController extends Controller
 {
-    public function index($id)
+    public function index($userid)
     {   
 
-        $result = DB::table('goals')->where('userno', $id)->where('deleted_at', null)->get();
+        $result = DB::table('goals')->where('userid', $userid)->where('deleted_at', null)->get();
+
+        $startdate = $result->startperiod;
+        $enddate = $result->endperiod;
+        
+        
+
         return view('goal')->with('data', $result);
     }
 
@@ -60,5 +66,21 @@ public function delete($id, Request $Req){
     return view('goal')->with('data', $result);
 
 }
+// public function complete($id){
+
+//     $result = DB::table('goals')->where('userno', $id)->where('deleted_at', null)->get();
+//     return view('goal')->with('data', $result);
+
+
+//     $today = date();  // 현재 날짜
+//     $day = date('w');
+//     $currentYear = date('Y'); // 현재 년도
+//     $currentMonth = date('m'); // 현재 달
+
+//     $start = date('Y-m-d', strtotime($today." -".$day."days")); // 이번주 시작요일 계산
+//     $end = date('Y-m-d', strtotime($start." +6days")); // 이번주 마지막날 계산
+//     $startDate =date('m-d',strtotime($start));  // 형태를 월과 요일로 바꾸기
+//     $endDate =date('m-d',strtotime($end));  // 형태를 월과 요일로 바꾸기
+// }
 
 }
