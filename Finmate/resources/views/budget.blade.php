863e634f9bf70450a2160d6823d1d9cb4e523907
Headh1 <126057480+Headh1@users.noreply.github.com>
2023-06-24 15:56:00 +0900

    2023.06.24 budget,static up

--- Finmate/resources/views/budget.blade.php
+++ Finmate/resources/views/budget.blade.php
@@ -5,24 +5,24 @@
 @section('contents')
     <link rel="stylesheet" href="{{ asset('/css/test.css')  }}" >
     <main>
-    <a href="{{ route('budgetset.get') }}">예산 수정</a>
+    <a href="{{ route('budgetset.get') }}" class = "budget_a">예산 수정</a>
     <div class="monthBudget">
-        <p>{{$data[2]}}달 지출 현황 </p>
+        <p>{{$data['currentMonth']}}달 지출 현황 </p>
         <p>한달 예산 : {{number_format($all)}}원</p>
         <p>사용한 금액 : {{number_format($sumamount)}}원</p>
     </div>
     <br>
     <br>
     <p>주간 지출 현황</p>
-    <p>{{$data[0]}} ~ {{$data[1]}}</p>
+    <p>{{$data['startDate']}} ~ {{$data['endDate']}}</p>
     <br>
     <br>
     <div class="leftone">
         <p>남은금액</p>
-        <p>{{ $data[4]>0 ? number_format($data[4]) : "예산 금액을 초과하셨습니다." }}</p>
+        <p>{{ $data['leftBudget']>0 ? number_format($data['leftBudget'])."원" : "예산 금액을 초과하셨습니다." }}</p>
     </div>
-    <p>주간예산 : {{number_format($data[3])}}</p>
-    <p>사용금액 : {{number_format($sumweek)}}</p>
+    <p class="weekbudget">주간예산 : {{number_format($data['weekBudget'])}}원</p>
+    <p>사용금액 : {{number_format($sumweek)}}원</p>
     <br>
     </main>
 
